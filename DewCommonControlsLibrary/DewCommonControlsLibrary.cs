61c55e3a6769010787bf1d451b060f892682f86e
Andrea Vincenzo Abbondanza <andreabbondanza@outlook.com>
2016-08-21 08:49:05 +0200

    Added AppSettings helper to common library

--- DewCommonControlsLibrary/DewCommonControlsLibrary.cs
+++ DewCommonControlsLibrary/DewCommonControlsLibrary.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Storage;
 using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Data;
@@ -11,7 +12,138 @@ using Windows.UI.Xaml.Media.Animation;
 
 namespace DewCommonControlsLibrary
 {
+    namespace AppSettingsHelper
+    {
+        /// <summary>
+        /// Helper for AppSettings
+        /// </summary>
+        class AppSettings
+        {
+            /// <summary>
+            /// Set the local setting with the containskey control.
+            /// </summary>
+            /// <param name="name">The key name</param>
+            /// <param name="val">the value casted in object</param>
+            /// <param name="create">true = if not exists new key is create, else throw an exception</param>
+            /// <exception cref="InvalidOperationException"></exception>
+            public static void SetAppSettingsLocal(string name, object val, bool create = false)
+            {
+                if (ApplicationData.Current.LocalSettings.Values.ContainsKey(name))
+                    ApplicationData.Current.LocalSettings.Values[name] = val;
+                else
+                {
+                    if (create)
+                    {
+                        AppSettings.DefineAppSettingsLocal(name, val);
+                    }
+                    else
+                        throw new InvalidOperationException("Access without key definition");
 
+                }
+
+            }
+            /// <summary>
+            /// Get a local setting with containskey control.
+            /// </summary>
+            /// <param name="name">The key name</param>
+            /// <returns>Object of setting, null if not exist</returns>
+            public static object GetAppSettingsLocal(string name)
+            {
+                object Result = null;
+                if (ApplicationData.Current.LocalSettings.Values.ContainsKey(name))
+                    Result = ApplicationData.Current.LocalSettings.Values[name];
+                return Result;
+            }
+            /// <summary>
+            /// Create a new local setting
+            /// </summary>
+            /// <param name="name">The key name</param>
+            /// <param name="val">The value in object</param>
+            public static void DefineAppSettingsLocal(string name, object val)
+            {
+                ApplicationData.Current.LocalSettings.Values.Add(name, val);
+            }
+            /// <summary>
+            /// Check if a key exists
+            /// </summary>
+            /// <param name="name"></param>
+            /// <returns></returns>
+            public static bool AppSettingsLocalExists(string name)
+            {
+                return ApplicationData.Current.LocalSettings.Values.ContainsKey(name);
+            }
+            /// <summary>
+            /// Remove a local setting
+            /// </summary>
+            /// <param name="name">key</param>
+            public static void AppSettingsRemove(string name)
+            {
+                ApplicationData.Current.LocalSettings.Values.Remove(name);
+            }
+
+            /// <summary>
+            /// Set the local setting with the containskey control.
+            /// </summary>
+            /// <param name="name">The key name</param>
+            /// <param name="val">the value casted in object</param>
+            /// <param name="create">true = if not exists new key is create, else throw an exception</param>
+            /// <exception cref="InvalidOperationException"></exception>
+            public static void SetAppSettingsRoam(string name, object val, bool create = false)
+            {
+                if (ApplicationData.Current.RoamingSettings.Values.ContainsKey(name))
+                    ApplicationData.Current.RoamingSettings.Values[name] = val;
+                else
+                {
+                    if (create)
+                    {
+                        AppSettings.DefineAppSettingsRoam(name, val);
+                    }
+                    else
+                        throw new InvalidOperationException("Access without key definition");
+
+                }
+
+            }
+            /// <summary>
+            /// Get a local setting with containskey control.
+            /// </summary>
+            /// <param name="name">The key name</param>
+            /// <returns>Object of setting, null if not exist</returns>
+            public static object GetAppSettingsRoam(string name)
+            {
+                object Result = null;
+                if (ApplicationData.Current.RoamingSettings.Values.ContainsKey(name))
+                    Result = ApplicationData.Current.RoamingSettings.Values[name];
+                return Result;
+            }
+            /// <summary>
+            /// Create a new local setting
+            /// </summary>
+            /// <param name="name">The key name</param>
+            /// <param name="val">The value in object</param>
+            public static void DefineAppSettingsRoam(string name, object val)
+            {
+                ApplicationData.Current.RoamingSettings.Values.Add(name, val);
+            }
+            /// <summary>
+            /// Check if a key exists
+            /// </summary>
+            /// <param name="name"></param>
+            /// <returns></returns>
+            public static bool AppSettingsRoamExists(string name)
+            {
+                return ApplicationData.Current.LocalSettings.Values.ContainsKey(name);
+            }
+            /// <summary>
+            /// Remove a local setting
+            /// </summary>
+            /// <param name="name">key</param>
+            public static void AppSettingsRemoveRoam(string name)
+            {
+                ApplicationData.Current.RoamingSettings.Values.Remove(name);
+            }
+        }
+    }
     namespace DewCommonControlsLibraryConverters
     {
         /// <summary>
